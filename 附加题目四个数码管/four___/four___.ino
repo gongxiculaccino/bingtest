582bec2b0fc87c3a2a242a7c83cea0ea12070675
gongxiculaccino <52458635+gongxiculaccino@users.noreply.github.com>
2019-07-06 00:02:02 +0800

    Add files via upload

---
+++ 附加题目四个数码管/four___/four___.ino
@@ -0,0 +1,180 @@
+
+  
+  void setup()
+{
+  
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(10, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(10, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(11, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  delay(10);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+  
+}void setup()
+{
+  
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(10, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(10, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(11, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  delay(10);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+  
+}void setup()
+{
+  
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(10, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(10, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(11, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(4,LOW);
+  delay(10);
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  delay(10);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+  
+}
